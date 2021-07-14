4dfb5f0181eb98c5c43586c31766e84dd40389ea
anelisynagano <anelisynagano@gmail.com>
2021-07-14 15:30:31 +0100

    instructions

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-const myFunction = () => console.log("click!")
+document.addEventListener("DOMContentLoaded", () => {
+  // create a function called fetchData that will retrieve information from the url: https://thatcopy.pw/catapi/rest/
+  // for now, let's console.log what we retrieve from the database (make sure we have just the value of an url)
+
+
+  // create a function called renderImages that will receive an imgUrl as an argument
+  // store the div with id "container" in a variable
+  // create an image element
+
+  
+  // the source of the image created will be the one we receive as argument of our function
+  // add a classname to this image element called "image"
+  // append the image to the previously stored div "container"
+
+
+  // on our fetchData function, instead of console.log the information we get from the API, let's pass that info as argument to renderImages()
+  // call fetchData that will automatically run renderQuote() also
+
+});
